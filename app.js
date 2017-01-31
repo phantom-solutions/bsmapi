a5ca5c031074a13b541dcbf5db28638f1ec2ef03
Hunter Nakagawa <hunternakagawa@outlook.com>
2017-01-31 07:49:49 -0800

    added test minecraft files

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 var request = require('request');
+var db_middleware = require('./otherworld/db.middleware.js');
 // STILL HAVE YET TO WORK ON THIS BRB
--- otherworld/db.middleware.js
+++ otherworld/db.middleware.js
@@ -1,1 +1,3 @@
 // The Godfather of the app. The primary database interface for the API.
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./storage/saimin.db');
