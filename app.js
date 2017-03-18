36b31ac195375429a82411bfb3d3abcadbd60758
cyberstrawberry101 <cyberstrawberry101@gmail.com>
2017-03-18 12:38:01 -0600

    Formatted Server Handlers and Styles

--- app.js
+++ app.js
@@ -7,77 +7,109 @@ var express = require('express');
 var saimin = express();
 var config = require('./storage/config.json');
 const crypto = require('crypto');
-// Does simple checks to make sure things are A-okay.
-if(process.argv[2] == "test") {
-  if (fs.existsSync('node_modules')) {
-    console.log(chalk.green("[OK] node_modules exists."));
-  } else {
-    console.log(chalk.red("[FAIL] CONGRATULATIONS, YOU BROKE EVERYTHING. ASDFGHJKL;"));
-  }
-  if (fs.existsSync('storage/saimin.db')) {
-    console.log(chalk.green("[OK] database file exists."));
-    process.exit(0);
-  } else {
-    console.log(chalk.red("[FAIL] database file does not exist. creating one now."));
-    fs.closeSync(fs.openSync('storage/saimin.db', 'w'));
-    process.exit(1);
-  }
-}
-// Timestamp function that refuses to work.
-function timestamp(){
-  timestamp = new Date();
-  return timestamp.toLocaleString();
-};
-// :D
-saimin.all('/areyoualive', function (req, res) {
-  res.send("no");
-});
+
+
 // Fetches config files based on appid.
 saimin.get('/config/:appid', function (req, res) {
-  configdata = fs.readFile('./storage/configs/' + req.params.appid + '.json', 'utf8', (err, data) => {
-    console.log(chalk.yellow("[INFO](" + Date().toLocaleString() + ") Got request for " + req.params.appid));
-    if (err) {
-      res.send("404");
-      console.log(chalk.red("[ERROR](" + Date().toLocaleString() + ") Couldn't find it. :("));
-    } else {
-      res.send(data);
-      console.log(chalk.yellow("[INFO](" + Date().toLocaleString() + ") Found it. Sending."));
-    }
-  });
+configdata = fs.readFile('./storage/configs/' + req.params.appid + '.json', 'utf8', (err, data) => 
+{
+	console.log(chalk.bgGreen("[INFO]") + "(" + Date().toLocaleString() + ")" + chalk.bgBlue(" Got request for AppID: " + req.params.appid));
+	if (err) {
+		res.send("404");
+		console.log(chalk.bgRed("[ERROR]") +  "(" + Date().toLocaleString() + ")" + chalk.bgBlue(" Couldn't find AppID: " + req.params.appid));
+	} 
+	else 
+	{
+		res.send(data);
+		console.log(chalk.yellow("[INFO](" + Date().toLocaleString() + ") Found AppID: " + req.params.appid + " Sending Config Data..."));
+	}
 });
+});
+
+
+//==============================================================//
+// API REQUEST HANDLERS                                         //
+//==============================================================//
 // Gets the index of all configs.
 saimin.get('/index', function (req, res) {
-  var directory = "./storage/configs";
-  var indexlist = {}
-  fs.readdir( directory, function( err, files ) {
-          files.forEach(function(file, index) {
-            var indexid = file.replace(".json", "");
-            var jsonfile = require('./storage/configs/' + indexid + '.json');
-            indexlist[indexid] = jsonfile.SERVER_type;
-          });
-          res.json(indexlist);
-          console.log(chalk.yellow("[INFO](" + Date().toLocaleString() + ") Sending index."));
-  });
+var directory = "./storage/configs";
+var indexlist = {}
+fs.readdir( directory, function( err, files ) 
+{
+	files.forEach(function(file, index) 
+	{
+		var indexid = file.replace(".json", "");
+		var jsonfile = require('./storage/configs/' + indexid + '.json');
+		indexlist[indexid] = jsonfile.SERVER_type;
+    });
+        res.json(indexlist);
+        console.log(chalk.bgGreen("[INFO]") + "(" + Date().toLocaleString() + ")" + chalk.bgBlue(" Sending Index..."));
+	});
 });
-// Respond with 410 because yes.
+
+// Launch the integrated server.
+saimin.listen(config.port, config.ip, function () 
+{
+	console.log(chalk.bgGreen("[INFO]") + "(" + Date().toLocaleString() + ")" + chalk.bgBlue(" Borealis Server Manager API: Listening on Port " + config.port));
+});
+
+// Respond with 410.
 saimin.all('/', function (req, res) {
   res.send('410');
 })
-// Launch the integrated server.
-saimin.listen(config.port, config.ip, function () {
-  console.log(chalk.green("[INFO](" + Date().toLocaleString() + ") bsmapi listening on port " + config.port + "."));
+
+// Check for Alive Status
+saimin.all('/areyoualive', function (req, res)
+{
+	res.send("yes");
 });
-// Handle Ctrl+C
-if (process.platform === "win32") {
-  var rl = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.on("SIGINT", function () {
-    process.emit("SIGINT");
-  });
-}
+
+//==============================================================//
+// API SHUTDOWN HANDLERS                                        //
+//==============================================================//
+// Handle Server Shutdown
 process.on("SIGINT", function () {
-  console.log("Do not go gentle into that good night. Rage, rage against the dying of the light.");
+  console.log(chalk.bgRed("Borealis Server Manager API: Shutting Down..."));
   process.exit();
 });
+
+// Handle Ctrl+C
+if (process.platform === "win32") 
+{
+	var rl = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+	rl.on("SIGINT", function () 
+	{
+		process.emit("SIGINT");
+	});
+}
+
+
+//==============================================================//
+// API DATABASE HANDLERS		    		                    //
+//==============================================================//
+// Does simple checks to make sure things are A-okay.
+if(process.argv[2] == "test") {
+	if (fs.existsSync('node_modules')) 
+	{
+		console.log(chalk.green("[OK] node_modules exists."));
+	} 
+	else
+	{
+		console.log(chalk.red("[FAIL] CONGRATULATIONS, YOU BROKE EVERYTHING. ASDFGHJKL;"));
+	}
+	
+	if (fs.existsSync('storage/saimin.db')) 
+	{
+		console.log(chalk.green("[OK] database file exists."));
+		process.exit(0);
+	} 
+	else 
+	{
+		console.log(chalk.red("[FAIL] database file does not exist. creating one now."));
+		fs.closeSync(fs.openSync('storage/saimin.db', 'w'));
+		process.exit(1);
+	}
+}
