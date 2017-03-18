d938b6733f29a731cdb6b60f6521cce65d76af18
cyberstrawberry101 <cyberstrawberry101@gmail.com>
2017-03-18 12:57:27 -0600

    Finalized formatting and changes

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ const crypto = require('crypto');
 saimin.get('/config/:appid', function (req, res) {
 configdata = fs.readFile('./storage/configs/' + req.params.appid + '.json', 'utf8', (err, data) => 
 {
-	console.log(chalk.bgGreen("[INFO]") + " [" + Date().toLocaleString() + "] " + chalk.bgBlue("[Got request for AppID: " + req.params.appid + "]"));
+	console.log(chalk.bgGreen("[INFO]") + " (" + Date().toLocaleString() + ") " + chalk.bgBlue("[Got request for AppID: " + req.params.appid + "]"));
 	
 	if (err) {
-		console.log(chalk.bgRed("[ERROR]") + " [" + Date().toLocaleString() + "] " + chalk.bgBlue("[Couldn't locate AppID: " + req.params.appid + "]"));
+		console.log(chalk.bgRed("[ERROR]") + " (" + Date().toLocaleString() + ") " + chalk.bgBlue("[Couldn't locate AppID: " + req.params.appid + "]"));
 		res.send("404");
 	} 
 	else 
 	{
-		console.log(chalk.bgGreen("[INFO]") + " [" + Date().toLocaleString() + "] " + chalk.bgBlue("[Sending Config Data for AppID: " + req.params.appid + "]"));
+		console.log(chalk.bgGreen("[INFO]") + " (" + Date().toLocaleString() + ") " + chalk.bgBlue("[Sending Config Data for AppID: " + req.params.appid + "]"));
 		res.send(data);
 	}
 });
@@ -44,19 +44,20 @@ fs.readdir( directory, function( err, files )
 		indexlist[indexid] = jsonfile.SERVER_type;
     });
         res.json(indexlist);
-        console.log(chalk.bgGreen("[INFO]") + "(" + Date().toLocaleString() + ")" + chalk.bgBlue("Sending Index..."));
+        console.log(chalk.bgGreen("[INFO]") + " (" + Date().toLocaleString() + ") " + chalk.bgBlue("[Sending Index...]"));
 	});
 });
 
 // Launch the integrated server.
 saimin.listen(config.port, config.ip, function () 
 {
-	console.log(chalk.bgGreen("[INFO]") + "(" + Date().toLocaleString() + ")" + chalk.bgBlue("Borealis Server Manager API: Listening on Port " + config.port));
+	console.log(chalk.bgGreen("[INFO]") + " (" + Date().toLocaleString() + ") " + chalk.bgBlue("[Borealis Server Manager API: Listening on Port " + config.port + "]"));
 });
 
 // Respond with 410.
-saimin.all('/', function (req, res) {
-  res.send('410');
+saimin.all('/', function (req, res) 
+{
+	res.send('410');
 })
 
 // Check for Alive Status
@@ -70,8 +71,8 @@ saimin.all('/areyoualive', function (req, res)
 //==============================================================//
 // Handle Server Shutdown
 process.on("SIGINT", function () {
-  console.log(chalk.bgRed("Borealis Server Manager API: Shutting Down..."));
-  process.exit();
+	console.log(chalk.bgRed("[SHUTTING DOWN]") + " (" + Date().toLocaleString() + ") " + chalk.bgRed("[Borealis Server Manager API: Shutting Down...]"));
+	process.exit();
 });
 
 // Handle Ctrl+C
